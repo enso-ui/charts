ab7e22b40d88fb68711b067f27e3d9e059f229ad
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2021-06-03 15:25:19 +0300

    properly fixes labels update

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -110,11 +110,10 @@ export default {
 
             if (this.structureChanged()) {
                 this.$set(this.chart.data, 'datasets', this.data.datasets);
+                this.$set(this.chart.data, 'labels', this.data.labels);
             } else {
                 this.updateDatasets();
             }
-
-            this.$set(this.chart.data, 'labels', this.data.labels);
 
             this.chart.update();
         },
